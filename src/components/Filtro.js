250504407d9b40add81df601dfc75a9174387163
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-21 18:05:12 -0300

    construção-da--home

---
+++ src/components/Filtro.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Filtro extends Component {
+  render() {
+    return (
+      <>
+        <h1>olá,eu sou um filtro</h1>
+      </>
+    );
+  }
+}
+
