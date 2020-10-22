4e665e49fa358461cc4dfe65e346f1a6beb3f008
Wesley Aquino <lelinho93@gmail.com>
2020-10-22 14:37:02 -0300

    Estruturação estática do site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import Filtro from './components/Filtro';
-import Carrinho from './components/Carrinho';
+import { Filters } from './components/Filters/Filters';
+import { Products } from './components/Products/Products';
+import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
+import styled from 'styled-components';
 
-class App extends React.Component {
-  
-  render() {
+const AppContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 3fr 1fr;
+padding: 16px;
+gap: 10px;
+`
+
+function App() {
   return (
-    <div className="container-principal">
-      <h1>Bem vindo a LOJA ASTRODEV</h1> 
-      <div className="container-produtos">     
-        <Home texto={"Camiseta 1"} valor={70.00} foto={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2osbyk0xeMUiSmiDjiaHEvgoT1xkrORSJ5g&usqp=CAU"}/>
-        <Home texto={"Camiseta 2"} valor={70.00} foto={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEczzDxJOa8et8vXP-5ULM_yBuVcVoziVM_Q&usqp=CAU"}/>
-        <Home texto={"Camiseta 3"} valor={75.00}foto={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQTwEAtr8QnZXeEnLxRPj6zhtj9bx1QKBEQ8A&usqp=CAU"}/>
-        <Home texto={"Camiseta 4"} valor={80.00}foto ={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMITmylloZKZpovCgCzx2Q8nNY9S2LGdHYOMI3pKEQaH3iNkOe6iugIlG7h6yqbG7QSHPxkqA&usqp=CAc"}/>
-        <Home texto={"Camiseta 5"} valor={80.00}foto={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_xwB6ITpYn3oU3QoC-kxfLfBWsfYrNIBauu7zLhOj3KP4KZwqM3UxgR7SB0_1U4ry7qJYKy0&usqp=CAc"}/>
-        <Home texto={"Camiseta 6"} valor={60.00}foto={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1FESAFNHHqF6HX58XuQc4zkMJdI_Y8fzYPA&usqp=CAU"}/>  
-      </div>
-    </div>
+    <AppContainer>
+      <Filters />
+      <Products />
+      <ShoppingCart />
+    </AppContainer>
   );
-  } 
 }
 export default App;
-
