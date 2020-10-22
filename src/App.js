e0766ac6390a1dee14c6cc2d73cd1d357ad4eea3
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-22 16:28:45 -0300

    estruturando-o-site-2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,38 @@ padding: 16px;
 gap: 10px;
 `
 
+const Products = [
+
+  {
+    id: 1,
+    nome:"Camisa 1",
+    preco: 75.0,
+    imagem: "https://picsum.photos/100/150?random=1"
+  },
+
+  {
+    id: 2,
+    nome:"Camisa 2",
+    preco: 75.0,
+    imagem: "https://picsum.photos/100/150?random=2"
+  },
+
+  {
+    id: 3,
+    nome:"Camisa 3",
+    preco: 75.0,
+    imagem: "https://picsum.photos/100/150?random=3"
+  },
+
+  {
+    id: 4,
+    nome:"Camisa 4",
+    preco: 75.0,
+    imagem: "https://picsum.photos/100/150?random=4"
+  }
+
+]
+
 function App() {
   return (
     <AppContainer>
